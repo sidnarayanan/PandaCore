cae1f436cd2c5edba3f8755dae2defccc0637acd
Sid Narayanan <sidn@mit.edu>
2018-06-02 13:48:42 -0400

    truncate long names; I am distracted in the desert

--- Utils/interface/Logger.h
+++ Utils/interface/Logger.h
@@ -2,18 +2,12 @@
 #define LOGGER_H
 
 #include "TString.h"
+#define MAXLOGGERLEN 30
+
 
 class Logger {
 public:
-  Logger(TString name="") :
-    _isatty(isatty(fileno(stdout))),
-    _fhandle(_isatty ? stdout : stderr),
-    _name(name),
-    _info("INFO", "32m", _fhandle),
-    _debug("DEBUG", "36m", _fhandle),
-    _warning("WARNING", "91m", stderr),
-    _error("ERROR", "41m\033[1;37m", stderr) { }
-
+  Logger(TString name="");
   void info(const char *title, const char *msg, const char *n="\n") {
     _report(_info, title, msg, n);
   }
@@ -29,20 +23,16 @@ public:
 
 private:
   struct RType {
-    RType(TString n, TString c, FILE* f) : name(n), color(c), fhandle(f) {
-      pad = "";
-      for (int i = n.Length(); i != 9; ++i)
-        pad += " ";
-    }
+    RType(TString n, TString c, FILE* f);
     TString name;
     TString color;
     TString pad;
     FILE *const fhandle;
   };
-  void _report(const RType& r, const char *title, const char *msg, const char *n);
+  void _report(const RType& r, TString title, const TString& msg, const TString& n);
   bool _isatty;
   FILE *const _fhandle;
-  TString _name;
+  TString _name, _tmpl;
   RType _info, _debug, _warning, _error;
 };
 
--- Utils/src/Logger.cc
+++ Utils/src/Logger.cc
@@ -2,18 +2,46 @@
 
 Logger logger; 
 
-void Logger::_report(const RType& r, const char *title, const char *msg, const char *n) {
-    if (_isatty) {
-      fprintf(
-        r.fhandle,
-        "%s",
-        Form("\033[0;%s%s\033[0m%s [%-30s]: %s%s",
-             r.color.Data(), r.name.Data(), r.pad.Data(), (_name+title).Data(), msg, n));
-    } else {
-      fprintf(
-        r.fhandle,
-        "%s",
-        Form("%s%s [%-30s]: %s%s",
-             r.name.Data(), r.pad.Data(), (_name+title).Data(), msg, n));
-    }
+Logger::RType::RType(TString n, TString c, FILE* f) : name(n), color(c), fhandle(f) 
+{
+  pad = "";
+  for (int i = n.Length(); i != 9; ++i)
+    pad += " ";
+}
+
+Logger::Logger(TString name) :
+  _isatty(isatty(fileno(stdout))),
+  _fhandle(_isatty ? stdout : stderr),
+  _name(name),
+  _info("INFO", "32m", _fhandle),
+  _debug("DEBUG", "36m", _fhandle),
+  _warning("WARNING", "91m", stderr),
+  _error("ERROR", "41m\033[1;37m", stderr) 
+{
+  if (_isatty)  
+    _tmpl = "\033[0;%s%s\033[0m%s [%-"+TString::Format("%i",MAXLOGGERLEN)+"s]: %s%s";
+  else
+    _tmpl = "%s%s [%-"+TString::Format("%i",MAXLOGGERLEN)+"s]: %s%s";
+}
+
+void Logger::_report(const RType& r, TString title, const TString& msg, const TString& n) 
+{ 
+  title = _name+title; 
+  if (title.Length() > MAXLOGGERLEN) {
+    title = "..." + title(title.Length()-MAXLOGGERLEN+3,
+                           title.Length());
+  }
+  if (_isatty) {
+    fprintf(
+      r.fhandle,
+      "%s",
+      Form(_tmpl,
+           r.color.Data(), r.name.Data(), r.pad.Data(), title.Data(), msg.Data(), n.Data()));
+  } else {
+    fprintf(
+      r.fhandle,
+      "%s",
+      Form(_tmpl,
+           r.name.Data(), r.pad.Data(), title.Data(), msg.Data(), n.Data()));
+  }
 }
